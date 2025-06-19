7b826aec2a2b671c86182f8270a584c30762b3c9
Vedant Pareek <dunefro@gmail.com>
2025-06-19 12:19:22 +0530

    Pinning helm version to avoid error in blueprint addons

--- versions.tf
+++ versions.tf
@@ -5,5 +5,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.57"
     }
+    # Added here because the upstream provider breaks
+    # https://github.com/aws-ia/terraform-aws-eks-blueprints-addons/issues/452
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.9"
+    }
   }
 }
