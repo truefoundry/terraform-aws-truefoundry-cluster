b33de901ef030fdbf2640be51d02798dfb024fb1
Harshit Luthra <25671488+sachincool@users.noreply.github.com>
2025-07-16 17:58:52 +0530

    feat(eks.tf, locals.tf): enhance security group rules by merging additional rules and updating port ranges

--- locals.tf
+++ locals.tf
@@ -14,13 +14,23 @@ locals {
     },
     var.tags
   )
+  cluster_security_group_additional_rules = {
+    egress_nodes_ephemeral_ports_tcp = {
+      description                = "To node 1025-65535"
+      protocol                   = "tcp"
+      from_port                  = 1025
+      to_port                    = 65535
+      type                       = "egress"
+      source_node_security_group = true
+    }
+  }
 
   node_security_group_additional_rules = {
     ingress_self_all = {
       description = "Node to node all ports/protocols"
       protocol    = "-1"
       from_port   = 0
-      to_port     = 0
+      to_port     = 1024
       type        = "ingress"
       self        = true
     }
